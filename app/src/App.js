0e1e57f8efdc8aba774e97056d9091771c482cf1
sangmiiiiiin <kwk680037@gmail.com>
2024-08-23 11:37:38 +0900

    🎉 project start

---
+++ app/src/App.js
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router } from 'react-router-dom';
+import Main2 from "./component/main2";
+import './style/css/styles.css';
+
+
+function App() {
+  return (
+    <Router>
+      <Main2 />
+    </Router>
+  );
+}
+
+export default App;
